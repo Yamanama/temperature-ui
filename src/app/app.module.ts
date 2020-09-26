c3a767c598684a2ab0a880ccb64947e41df9b48e
Benjamin Yliniemi <benjamin.yliniemi@snhu.edu>
2020-09-26 00:20:15 -0400

    Cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { GaugeComponent } from './gauge/gauge.component';
-import { ChartComponent } from './chart/chart.component';
 import { ChartsModule } from 'ng2-charts';
-import { RadarComponent } from './radar/radar.component';
 import { TemperatureComponent } from './temperature/temperature.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GaugeComponent,
-    ChartComponent,
-    RadarComponent,
     TemperatureComponent
   ],
   imports: [
